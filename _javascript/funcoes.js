242ef73385a4cc794ee3935998bd51173604534f
RafaelRCLima <rafael.lima19-92@hotmail.com>
2019-10-28 23:51:20 -0300

    Base

---
+++ _javascript/funcoes.js
@@ -0,0 +1,15 @@
+function mudaFoto(foto) {
+    document.getElementById("icone").src = foto;
+}
+
+function mudaImagem(img) {
+    document.getElementById("ppf").src = "_imagens/" + img + ".jpg";
+}
+
+function mudaImagem2(img) {
+    document.getElementById("gugu").src = "_imagens/" + img + ".jpg";
+}
+
+function mudaImagem3(img) {
+    document.getElementById("eu").src = "_imagens/" + img + ".jpg";
+}
